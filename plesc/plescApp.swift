a7e5f46fbcb103c187689cb64e50524fefd85669
a3p <me@a3p.re>
2025-03-27 15:26:12 +0000

    Initial Commit

---
+++ plesc/plescApp.swift
@@ -0,0 +1,17 @@
+//
+//  plescApp.swift
+//  plesc
+//
+//  Created by Matt Ball on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct plescApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plescTests/plescTests.swift
@@ -0,0 +1,17 @@
+//
+//  plescTests.swift
+//  plescTests
+//
+//  Created by Matt Ball on 27/03/2025.
+//
+
+import Testing
+@testable import plesc
+
+struct plescTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
